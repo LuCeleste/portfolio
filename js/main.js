9e14b82c68c8bd4b1709b4cb68d2001e1c764740
lulceles <lucianaalonso98@gmail.com>
2022-06-27 19:10:20 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let nombreIngresado = prompt("Ingrese su nombre");
+let salida  = "¡Hola, " + nombreIngresado + "!";
+alert(salida);
+
+let numero = parseInt(prompt("Ingrese un número"));
+let numeroDos = 25;
+let resultadoNumeros  = numero + numeroDos ;
+alert(resultadoNumeros);
+
+
+let textoUno = prompt("Dame un texto");
+let textoDos = prompt("Dame otro");
+let resultadoTextos  = textoUno + " " + textoDos ;
+alert(resultadoTextos);
